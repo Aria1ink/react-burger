17205229245037d56abd0c8b7fb4333b86629531
Aria1ink <107484275+Aria1ink@users.noreply.github.com>
2023-01-11 15:04:08 +0300

    header through pain and suffering

---
+++ react-burger/src/components/App/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppHeader from '../AppHeader/AppHeader';
+import BurgerConstructor from '../BurgerConstructor/BurgerConstructor';
+
+export default function App () {
+
+  return (
+    <>
+      <AppHeader />
+      <BurgerConstructor />
+    </>
+  )
+};
