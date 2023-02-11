dae990bb604bc9c5ec952d60a47f9fa34058f7f0
Aria1ink <107484275+Aria1ink@users.noreply.github.com>
2023-02-12 02:31:36 +0300

    part 4

--- react-burger/src/components/CartElement/CartElement.jsx
+++ react-burger/src/components/CartElement/CartElement.jsx
@@ -1,20 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { useDrag } from "react-dnd";
+import React from "react";
+import { useDrag, useDrop } from "react-dnd";
 import style from "./CartElement.module.css";
 
 import { ConstructorElement, DragIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 export default function CartElement(props) {
 const ingredient = props.item;
+const cartId = props.id;
 const [{isDrag}, dragRef] = useDrag({
-  type: "ingredient",
-  item: {cartId: props.id, ingredient: ingredient},
+  type: "cartItem",
+  item: {cartId: cartId, ingredient: ingredient},
   collect: monitor => ({
       isDrag: monitor.isDragging()
   })
 });
+const [dropTargetProps, dropTarget] = useDrop({
+  accept: "cartItem",
+  drop(item) {
+    onDropHandler(item);
+  },
+});
+const onDropHandler = (item) => {
+  console.log(item)
+  console.log({cartId: cartId, ingredient: ingredient})
+}
 
 return (              
-  <li ref={dragRef} className={style.item + " " + props.className} style={ !isDrag ? {opacity: "1"} : {opacity: "0.4", cursor: "grab"} }>
+  <li ref={(el)=> {dragRef(el); dropTarget(el);}} className={style.item + " " + props.className} style={ !isDrag ? {opacity: "1"} : {opacity: "0.4", cursor: "grab"} }>
     { ingredient.type !== "bun" && 
       <DragIcon type={"primary"} />
     }
