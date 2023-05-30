63ad020868693907eeb8c8b349e8dd0cc14fce5d
Aria1ink <107484275+Aria1ink@users.noreply.github.com>
2023-05-30 16:31:07 +0300

    fix

--- react-burger/src/components/IngredientImage/IngredientImage.jsx
+++ react-burger/src/components/IngredientImage/IngredientImage.jsx
@@ -4,7 +4,7 @@ import style from "./IngredientImage.module.css";
 export default function IngredientImage({url, custom}) {
 
   return(
-    <div className={style.imageContainer} style={custom}>
+    <div className={style.imageContainer } >
       <img src={url} className={style.image}/>
     </div>
   );
